a4cb1b44ef066706b70f1e56f85f1ae5776617a4
Marcelo Barsotti <mbarsott@gmail.com>
2019-05-03 21:27:29 -0400

    3.4. Identity and Security / [Authorize]

---
+++ Identity/Identity/Controllers/SecretController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace Identity.Controllers
+{
+//    [Authorize(Roles="admin, sales")]
+    [Authorize(Users="mbarsott@gmail.com")]
+    public class SecretController : Controller
+    {
+//        [Authorize]
+        public ContentResult Secret()
+        {
+            return Content("This is a secret...");
+        }
+
+        [AllowAnonymous]
+        public ContentResult Overt()
+        {
+            return Content("This is not a secret...");
+        }
+    }
+}
