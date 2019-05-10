247a4be06be998e770ed9225c7aaf1fefc3b6e63
Marcelo Barsotti <marcelo.barsotti@assentcompliance.com>
2019-05-10 16:50:36 -0400

    6.4. Entity Framework 6 / Multiple Migrations

---
+++ Books/Books.Web/DataContexts/BooksDb.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using Books.Entities;
+
+namespace Books.Web.DataContexts
+{
+    public class BooksDb : DbContext
+    {
+        public BooksDb() : base("DefaultConnection")
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ Books/Books.Web/DataContexts/IdentityDb.cs
@@ -0,0 +1,18 @@
+﻿using Books.Web.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Books.Web.DataContexts
+{
+    public class IdentityDb : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityDb()
+            : base("DefaultConnection", false)
+        {
+        }
+
+        public static IdentityDb Create()
+        {
+            return new IdentityDb();
+        }
+    }
+}
