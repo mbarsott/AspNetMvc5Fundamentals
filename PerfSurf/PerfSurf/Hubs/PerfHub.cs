3f4305b2fd151fef8572f5ba5565d09b6a9ad131
Marcelo Barsotti <mbarsott@gmail.com>
2019-05-13 07:43:41 -0400

    7.5 SignalR / Being Chatty

--- PerfSurf/PerfSurf/Hubs/PerfHub.cs
+++ PerfSurf/PerfSurf/Hubs/PerfHub.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Microsoft.AspNet.SignalR;
+﻿using Microsoft.AspNet.SignalR;
 
 namespace PerfSurf.Hubs
 {
     public class PerfHub : Hub
     {
-        public void Hello()
+        public void Send(string message)
         {
-            Clients.All.hello("Hi!");
+            Clients.All.newMessage(Context.User.Identity.Name + " says " + message);
         }
     }
 }
--- PerfSurf/PerfSurf/Scripts/PerfSurf.js
+++ PerfSurf/PerfSurf/Scripts/PerfSurf.js
@@ -1,1 +1,33 @@
-﻿
+﻿(function () {
+    var perfHub = $.connection.PerfHub;
+    $.connection.logging = true;
+    $.connection.hub.start();
+
+    var Model = function () {
+        var self = this;
+        self.message = ko.observable("");
+        self.messages = ko.observableArray();
+    }
+
+    Model.prototype = {
+        sendMessage: function () {
+            var self = this;
+            perfHub.server.send(self.message());
+            self.message("");
+        },
+        addMessage: function(message) {
+            var self = this;
+            self.messages.push(message);
+        }
+    };
+
+    var model = new Model();
+    $(function() {
+        ko.applyBindings(model);
+    });
+
+    perfHub.client.newMessage = function (message) {
+        model.addMessage(message);
+    };
+
+}());
